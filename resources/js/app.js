00ccb0ba27e9267dea69871153746c9e0f3ec952
sajjadshuvo <sajjadshuvo1212@gmail.com>
2024-09-26 01:28:08 +0600

    vue set up complete with laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,13 @@
 import './bootstrap';
+import { createApp } from 'vue';
+
+/**
+ * Next, we will create a fresh Vue application instance. You may then begin
+ * registering components with the application instance so they are ready
+ * to use in your application's views. An example is included for you.
+ */
+
+import App from './components/App.vue';
+const app = createApp(App);
+
+app.mount('#app');
